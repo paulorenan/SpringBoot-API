251a9e913a36a8605f8bcb6b1cfb32199c95b22c
Paulo Renan Nascimento de Almeida <able.paulorenan@gmail.com>
2022-06-14 12:14:38 -0300

    adicina devtools

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -6,18 +6,21 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 
   @RequestMapping("/topicos")
   @ResponseBody
   public List<Topico> lista() {
     Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programacao Java"));
-    return Arrays.asList(topico, topico, topico);
+    Topico topico2 = new Topico("Relacionamentos", "Relacionamentos entre Spring e Banco de Dados", new Curso("Spring", "Programacao Java"));
+    Topico topico3 = new Topico("Questão", "Questão com Spring", new Curso("Spring", "Programacao Java"));
+    return Arrays.asList(topico, topico2, topico3);
   }
   
 }
