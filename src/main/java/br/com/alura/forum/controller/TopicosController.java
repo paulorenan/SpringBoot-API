4f4fe36a2c6b2470781b8de40e166c20049f5ace
Paulo Renan Nascimento de Almeida <able.paulorenan@gmail.com>
2022-06-14 16:24:20 -0300

    adiciona application properties

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
@@ -16,11 +17,11 @@ public class TopicosController {
 
   @RequestMapping("/topicos")
   @ResponseBody
-  public List<Topico> lista() {
+  public List<TopicoDto> lista() {
     Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programacao Java"));
     Topico topico2 = new Topico("Relacionamentos", "Relacionamentos entre Spring e Banco de Dados", new Curso("Spring", "Programacao Java"));
     Topico topico3 = new Topico("Questão", "Questão com Spring", new Curso("Spring", "Programacao Java"));
-    return Arrays.asList(topico, topico2, topico3);
+    return TopicoDto.converter(Arrays.asList(topico, topico2, topico3));
   }
   
 }
