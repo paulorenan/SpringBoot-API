aaf21b094fdc955e19f38a38271309b8b1259f56
Paulo Renan Nascimento de Almeida <able.paulorenan@gmail.com>
2022-06-14 16:46:04 -0300

    banco de dados funcional

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO USUARIO (nome, email, senha) VALUES ('Renan', 'renan@gmail.com', '123456');
+
+INSERT INTO CURSO(nome, categoria) VALUES ('Java', 'Programação');
+INSERT INTO CURSO(nome, categoria) VALUES ('Python', 'Programação');
+
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES ('Topico 1', 'Mensagem 1', '2020-01-01', 'Aberto', 1, 1);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES ('Topico 2', 'Mensagem 2', '2020-01-01', 'Aberto', 1, 1);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES ('Topico 3', 'Mensagem 3', '2020-01-01', 'Aberto', 1, 1);
