152d794894a2ac2c60bcfafe48521833677194d8
Paulo Renan Nascimento de Almeida <able.paulorenan@gmail.com>
2022-06-15 12:22:24 -0300

    fazendo post com insominia

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,11 +1,14 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.controller.form.TopicoForm;
@@ -29,15 +32,19 @@ public class TopicosController {
   @GetMapping
   public List<TopicoDto> lista(String nomeCurso) {
     if (nomeCurso == null) {
-      return TopicoDto.converter(topicoRepository.findAll());
+      List<Topico> topicos = topicoRepository.findAll();
+      return TopicoDto.converter(topicos);
     } else {
       return TopicoDto.converter(topicoRepository.findByCursoNome(nomeCurso));
     }
   }
   
   @PostMapping
-  public void cadastrar(@RequestBody TopicoForm form) {
+  public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
     Topico topico = form.converter(cursoRepository);
     topicoRepository.save(topico);
+
+    URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+    return ResponseEntity.created(uri).body(new TopicoDto(topico));
   }
 }
