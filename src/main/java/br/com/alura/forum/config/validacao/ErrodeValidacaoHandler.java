8baf2deacaf512afeb673a8dc44dcd76fce5ea7c
Paulo Renan Nascimento de Almeida <able.paulorenan@gmail.com>
2022-06-16 10:16:04 -0300

    delete

--- src/main/java/br/com/alura/forum/config/validacao/ErrodeValidacaoHandler.java
+++ src/main/java/br/com/alura/forum/config/validacao/ErrodeValidacaoHandler.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ErrodeValidacaoHandler {
+public class ErroDeValidacaoHandler {
 
   @Autowired
   private MessageSource messageSource;
--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,10 +3,12 @@ package br.com.alura.forum.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +17,13 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.DetalhesDoTopicoDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -44,6 +48,7 @@ public class TopicosController {
   }
   
   @PostMapping
+  @Transactional
   public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
     Topico topico = form.converter(cursoRepository);
     topicoRepository.save(topico);
@@ -57,4 +62,20 @@ public class TopicosController {
     Topico topico = topicoRepository.getOne(id);
     return new DetalhesDoTopicoDto(topico);
   }
+
+  @PutMapping("/{id}")
+  @Transactional
+  public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+    Topico topico = form.atualizar(id, topicoRepository);
+
+    return ResponseEntity.ok(new TopicoDto(topico));
+  }
+
+  @DeleteMapping("/{id}")
+  @Transactional
+  public ResponseEntity<TopicoDto> remover(@PathVariable Long id) {
+    topicoRepository.deleteById(id);
+    return ResponseEntity.ok().build();
+  }
+
 }
